9833443eeb8d0b87e44ee1f5b91fadb53ac9fdd1
zhiyong <1757826934@qq.com>
2019-06-19 14:13:55 +0800

    审批侧边栏

--- src/components/drawer/drawer.vue
+++ src/components/drawer/drawer.vue
@@ -1,10 +1,13 @@
 <style lang="less">
+@import "~@/utils/utils.less";
 .approval-content{
   // padding: 24px;
   color: rgb(0,0,0,0.65);
   .approval-content__title{
     font-size: 16px;
-    color: rgb(0,0,0,0.65)
+    color: rgb(0,0,0,0.65);
+    display: flex;
+    align-items: center;
   }
   // 细的分割线
   .divider-horizontal{
@@ -58,12 +61,89 @@
     }
   }
   .approval__status{
+    margin: 0 auto;
+    width: 400px;
     display: flex;
     justify-content: space-between;
-    margin: 24px 0;
+    padding: 16px 8px;
+    &.title-submit{
+      padding: 0 8px;
+      background: #fafafa;
+      height: 40px;
+      line-height: 40px;
+    }
   }
 }
 
+.content-box{
+    margin: 0 auto;
+  margin-bottom: 24px;
+  p{
+    padding: 0;
+    margin: 0;
+  }
+  width: 400px;
+  // width: 100%;
+  color: rgba(0,0,0,.85);
+  border:0;
+  border-top: 1px;
+  border-left: 1px;
+  border-style: solid;
+  border-color: #E5E5E5;
+  .table-lable, .table-content{
+    box-sizing: border-box;
+    // float: left;
+    display: inline-block;
+    line-height: 1.5;
+    padding: 0 16px;
+  }
+  .table-lable{
+    width: 80px;
+    // width: calc(100%-318px);
+  }
+  .table-content{
+    width: 318px;
+    padding:0;
+    box-sizing: border-box;
+    // width: calc(100%-80px);
+    border: 0;
+    border-left: 1px;
+    border-style: solid;
+    border-color: #E5E5E5;
+  }
+  .first-row, .second-row, .third-row{
+    .clearfix();
+    border: 0;
+    box-sizing: border-box;
+    border-right: 1px;
+    border-bottom: 1px;
+    border-style: solid;
+    border-color: #E5E5E5;
+    display: flex;
+    align-items: center;
+    .table-content{
+      padding: 8px 16px;
+      &.doc-wrap{
+        padding: 0;
+        .doc{
+           padding: 8px 16px;
+          &.doc--line{
+            width: 100%;
+            border: 0;
+            border-bottom: 1px;
+            border-style: solid;
+            border-color: #E5E5E5;
+            position: relative;
+          }
+          .look{
+            margin: 0 16px;
+          }
+        }
+      }
+    }
+  }
+
+}
 </style>
 
 <template>
@@ -77,7 +157,7 @@
       <div class="approval-content">
         <div class="common-title">
           <div class="approval-content__title">
-            状态
+            <a-icon type="codepen-circle" style="font-size:32px;margin-right:8px "/>状态
           </div>
           <div class="divider-horizontal"></div>
           <div class="first-item">
@@ -107,7 +187,7 @@
         <div class="shallow-horizontal"></div>
         <div class="common-title">
           <div class="approval-content__title">
-            操作
+            <a-icon type="behance-square" style="font-size:32px;margin-right:8px "/>操作
           </div>
           <div class="divider-horizontal"></div>
           <a-form @submit="handleSubmit" :form="form" >
@@ -158,22 +238,190 @@
         <div class="shallow-horizontal"></div>
         <div class="common-title">
           <div class="approval-content__title">
-            历史
+            <a-icon type="slack" style="font-size:32px;margin-right:8px "/>
+            <span>
+              历史
+            </span>
           </div>
           <div class="divider-horizontal"></div>
+          <!-- 同意 -->
           <div class="approval__status">
             <span style="color:#52C41A">同意</span>
             <span>2017-12-14  &nbsp;09:00</span>
           </div>
-          <a-table :columns="columns" :dataSource="data" bordered>
-            <template slot="name" slot-scope="text">
-              <a href="javascript:;">{{ text }}</a>
-            </template>
-            <!-- <template slot="title" slot-scope="currentPageData">
-              Header
-            </template> -->
-          </a-table>
+          <div class="content-box">
+            <div class="first-row">
+              <div class="table-lable">
+                <span>
+                  审核人
+                </span>
+              </div>
+              <div class="table-content">
+                名称(职位)
+              </div>
+            </div>
+            <div class="second-row">
+              <div class="table-lable">
+                备注
+              </div>
+              <div class="table-content">
+                <p>
+                  审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容。
+                </p>
+              </div>
+            </div>
+            <div class="third-row">
+              <div class="table-lable">
+                附件
+              </div>
+              <div class="table-content doc-wrap">
+                <div class="doc">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                  <a href="javascript:;" class="look">预览</a>
+                  <a href="javascript:;">下载</a>
+                </div>
+                <div class="doc">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                  <a href="javascript:;" class="look">预览</a>
+                  <a href="javascript:;">下载</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- 驳回 -->
+          <div class="approval__status">
+            <span style="color:#F5222D">驳回</span>
+            <span>2017-12-14  &nbsp;09:00</span>
+          </div>
+          <div class="content-box">
+            <div class="second-row">
+              <div class="table-lable">
+                备注
+              </div>
+              <div class="table-content">
+                <p>
+                  审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容。
+                </p>
+              </div>
+            </div>
+            <div class="third-row">
+              <div class="table-lable">
+                附件
+              </div>
+              <div class="table-content doc-wrap">
+                <div class="doc doc--line">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                  <!-- <a href="javascript:;" class="look">预览</a>
+                  <a href="javascript:;">下载</a> -->
+                </div>
+                <div class="doc">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                  <!-- <a href="javascript:;" class="look">预览</a>
+                  <a href="javascript:;">下载</a> -->
+                </div>
+              </div>
+            </div>
+            <div class="first-row">
+              <div class="table-lable">
+                <span>
+                  审核人
+                </span>
+              </div>
+              <div class="table-content">
+                名称(职位)
+              </div>
+            </div>
 
+          </div>
+          <!-- 提交 -->
+          <div class="approval__status title-submit">
+            <span style="color:#FACC14">提交</span>
+            <span>2017-12-14  &nbsp;09:00</span>
+          </div>
+          <div class="content-box">
+            <div class="second-row">
+              <div class="table-lable">
+                备注
+              </div>
+              <div class="table-content">
+                <p>
+                  审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容。
+                </p>
+              </div>
+            </div>
+            <div class="third-row">
+              <div class="table-lable">
+                附件
+              </div>
+              <div class="table-content doc-wrap">
+                <div class="doc doc--line">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                </div>
+                <div class="doc">
+                  <i class="anticon anticon-paper-clip" style="color: rgba(0, 0, 0, 0.45);"><svg
+                    viewBox="64 64 896 896"
+                    data-icon="paper-clip"
+                    width="1em"
+                    height="1em"
+                    fill="currentColor"
+                    aria-hidden="true"
+                    class=""><path d="M779.3 196.6c-94.2-94.2-247.6-94.2-341.7 0l-261 260.8c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l261-260.8c32.4-32.4 75.5-50.2 121.3-50.2s88.9 17.8 121.2 50.2c32.4 32.4 50.2 75.5 50.2 121.2 0 45.8-17.8 88.8-50.2 121.2l-266 265.9-43.1 43.1c-40.3 40.3-105.8 40.3-146.1 0-19.5-19.5-30.2-45.4-30.2-73s10.7-53.5 30.2-73l263.9-263.8c6.7-6.6 15.5-10.3 24.9-10.3h.1c9.4 0 18.1 3.7 24.7 10.3 6.7 6.7 10.3 15.5 10.3 24.9 0 9.3-3.7 18.1-10.3 24.7L372.4 653c-1.7 1.7-2.6 4-2.6 6.4s.9 4.7 2.6 6.4l36.9 36.9a9 9 0 0 0 12.7 0l215.6-215.6c19.9-19.9 30.8-46.3 30.8-74.4s-11-54.6-30.8-74.4c-41.1-41.1-107.9-41-149 0L463 364 224.8 602.1A172.22 172.22 0 0 0 174 724.8c0 46.3 18.1 89.8 50.8 122.5 33.9 33.8 78.3 50.7 122.7 50.7 44.4 0 88.8-16.9 122.6-50.7l309.2-309C824.8 492.7 850 432 850 367.5c.1-64.6-25.1-125.3-70.7-170.9z"></path></svg></i>
+                  <span style="margin-left:8px">文档.docx</span>
+                </div>
+              </div>
+            </div>
+            <div class="first-row">
+              <div class="table-lable">
+                <span>
+                  审核人
+                </span>
+              </div>
+              <div class="table-content">
+                名称(职位)
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
 
@@ -181,30 +429,7 @@
   </div>
 </template>
 <script>
-const columns = [{
-  title: '审核人',
-  dataIndex: 'name',
-  scopedSlots: { customRender: 'name' }
-}, {
-  title: '名称(职位)',
-  className: 'column-money',
-  dataIndex: 'money'
-}]
 
-const data = [{
-  key: '1',
-  name: '备注',
-  money: '审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容审核内容',
-  address: 'New York No. 1 Lake Park'
-}, {
-  key: '2',
-  name: '附件',
-  money: '￥1,256,000.00'
-}, {
-  key: '3',
-  name: 'Joe Black',
-  money: '￥120,000.00'
-}]
 export default {
   name: 'Drawer',
   props: {
@@ -216,8 +441,6 @@ export default {
   },
   data () {
     return {
-      data,
-      columns,
       labelCol: { lg: { span: 5 }, sm: { span: 5 } },
       wrapperCol: { lg: { span: 19 }, sm: { span: 19 } },
       fileList: [{
@@ -227,7 +450,6 @@ export default {
         url: 'http://www.baidu.com/xxx.png'
       }],
       form: this.$form.createForm(this)
-      // visible: false
     }
   },
   methods: {

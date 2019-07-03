4951a5689df7a35d3a0fa4b8623339cddd48daeb
makui <1606588217@qq.com>
2019-07-03 14:27:30 +0800

    放大镜

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="user-wrapper">
     <div class="content-box">
-      <span class="antd-pro-components-global-header-index-action antd-pro-components-global-header-index-search antd-pro-components-header-search-index-headerSearch">
-
+      <span class="action" @click="bigScale">
+        <a-icon type="zoom-in"/>
       </span>
       <a href="/help/children/first" target="_blank">
         <span class="action">
@@ -42,50 +42,185 @@
         </a-menu>
       </a-dropdown>
       <span class="action" @click="openDrawer">
-        <a-icon type="setting" />
+        <a-icon type="setting"/>
       </span>
     </div>
   </div>
 </template>
 
 <script>
-import NoticeIcon from '@/components/NoticeIcon'
-import { mapActions, mapGetters } from 'vuex'
+  import NoticeIcon from '@/components/NoticeIcon'
+  import { mapActions, mapGetters } from 'vuex'
+  import $ from 'jquery'
 
-export default {
-  name: 'UserMenu',
-  components: {
-    NoticeIcon
-  },
-  methods: {
-    ...mapActions(['Logout', 'visible']),
-    ...mapGetters(['nickname', 'avatar']),
-    handleLogout () {
-      const that = this
+  export default {
+    name: 'UserMenu',
+    components: {
+      NoticeIcon
+    },
+    methods: {
+      ...mapActions(['Logout', 'visible']),
+      ...mapGetters(['nickname', 'avatar']),
+      handleLogout() {
+        const that = this
 
-      this.$confirm({
-        title: '提示',
-        content: '真的要注销登录吗 ?',
-        onOk () {
-          return that.Logout({}).then(() => {
-            // debugger
-            window.location.reload()
-          }).catch(err => {
-            that.$message.error({
-              title: '错误',
-              description: err.message
+        this.$confirm({
+          title: '提示',
+          content: '真的要注销登录吗 ?',
+          onOk() {
+            return that.Logout({}).then(() => {
+              // debugger
+              window.location.reload()
+            }).catch(err => {
+              that.$message.error({
+                title: '错误',
+                description: err.message
+              })
+            })
+          },
+          onCancel() {
+          }
+        })
+      },
+      openDrawer() {
+        this.$store.dispatch('ToggleSetDrawer', true)
+      },
+      bigScale() {
+        $.fn.BUP = function(scale, className) {
+          let $element = this
+          let $className = className
+          let $BOX_WIDTH = $element.width()
+          let $BOX_HEIGHT = $element.height()
+          let $options = {
+            round: false,
+            width: 400,
+            height: 200,
+            background: '#FFF',
+            shadow: '0 8px 17px 0 rgba(0, 0, 0, 1)',
+            border: '3px solid #FFF',
+            cursor: true,
+            zIndex: 9999999
+          }
+
+          $element.on('dragstart', function(e) {
+            e.preventDefault()
+          })
+
+          // Create element
+          let lens = document.createElement('div')
+          lens.id = '' + $className + 'BlowupLens'
+          let mask = document.createElement('div')
+          mask.id = '' + $className + 'BlowupMask'
+
+          // Attack the element to the body
+          $('body').append(lens)
+          $('body').append(mask)
+          let $blowupMask = $('#' + mask.id)
+          $blowupMask.css({
+            'cursor': $options.cursor ? 'crosshair' : 'none',
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'right': '0',
+            'bottom': '0',
+            'background': 'transparent',
+            'z-index': '100000'
+          })
+          let el = $element.clone(true).css({
+            'width': ($BOX_WIDTH) + 'px',
+            'height': ($BOX_HEIGHT) + 'px',
+            'transform': 'scale(' + scale + ')',
+            'transform-origin': 'top left',
+            'position': 'relative'
+          })
+          $(lens).append(el)
+          // Updates styles
+          let $blowupLens = $('#' + lens.id)
+          $blowupLens.css({
+            'overflow': 'hidden',
+            'position': 'absolute',
+            'visibility': 'hidden',
+            'pointer-events': 'none',
+            'zIndex': $options.zIndex,
+            'width': $options.width,
+            'height': $options.height,
+            'border': $options.border,
+            'background': $options.background,
+            'border-radius': $options.round ? '50%' : 'none',
+            'box-shadow': $options.shadow
+          })
+
+          // 尝试使用滚轮控制放大 wheelDelta 兼容性问题
+          document.onmousewheel = function(e) {
+            e = event || window.event
+            console.log(e.wheelDelta)
+            if (e.wheelDelta === 120) {
+              scale += 0.1
+              if (scale >= 5) {
+                scale = 5
+                console.log('已经最大了')
+              }
+              console.log('Q')
+            }
+            if (e.wheelDelta === -120) {
+              scale -= 0.1
+              if (scale <= 1) {
+                scale = 1
+                console.log('已经最小了')
+              }
+              console.log('H')
+            }
+            console.log(scale)
+            $blowupLens.children().css({
+              'transform': 'scale(' + scale + ')'
+            })
+
+          }
+
+          // Show magnification lens
+          $blowupMask.mouseenter(function() {
+            $blowupLens.css('visibility', 'visible')
+          })
+
+          // Mouse motion on el
+          $blowupMask.mousemove(function(e) {
+            //
+            let lensX = e.pageX - $options.width / 2
+            let lensY = e.pageY - $options.height / 2
+
+            // Zoomed  coordinates
+            let zoomX = -Math.floor(lensX * scale) - ($options.width / 2) * (scale - 1)
+            let zoomY = -Math.floor(lensY * scale) - ($options.height / 2) * (scale - 1)
+
+            // Apply styles to lens
+            $blowupLens.css({
+              left: lensX,
+              top: lensY
+            })
+            $blowupLens.children().css({
+              left: zoomX,
+              top: zoomY
             })
           })
-        },
-        onCancel () {
+          //
+          // mouse leave
+          $blowupMask.mouseleave(function() {
+            $blowupLens.css('visibility', 'hidden')
+          })
+          // move defaults contextmenu
+          $blowupMask.bind('contextmenu', function() {
+            return false
+          })
+          $blowupMask.contextmenu(function() {
+            document.onmousewheel = null
+            $blowupMask.remove()
+            $blowupLens.remove()
+          })
         }
-      })
-    },
-    openDrawer () {
-      console.log(this.$router)
-      this.$store.dispatch('ToggleSetDrawer', true)
-      // this.$router.push({name:'login2'})
+        setTimeout(function() {
+          $('#app').BUP(1, 'App')
+        }, 1000)
+      }
     }
   }
-}
 </script>

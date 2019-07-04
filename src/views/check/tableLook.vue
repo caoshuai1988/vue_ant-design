d2b656fb9d814667151266772219ad01a78e799f
makui <1606588217@qq.com>
2019-07-04 14:07:35 +0800

    审批页弹出查看 增加放大镜

--- src/views/check/tableLook.vue
+++ src/views/check/tableLook.vue
@@ -5,10 +5,24 @@
         <h1 style="font-size: 20px;">{{ title }}</h1>
       </div>
       <div slot="extra">
-        <a-button-group >
-          <a-button value="narrow" @click="handleSizeChange"><a-icon type="zoom-out" /><span>缩 小</span></a-button>
+        <a-tooltip placement="bottom">
+          <a-button icon="zoom-in" style="margin-right: 8px" @click="bigScale">放大镜</a-button>
+          <template slot="title">
+            增加倍数：D<br/>
+            缩小倍数：A<br/>
+            放大可视区：W<br/>
+            缩小可视区：S <br/>
+            关闭放大镜：右键 Esc
+          </template>
+        </a-tooltip>
+        <a-button-group>
+          <a-button value="narrow" @click="handleSizeChange">
+            <a-icon type="zoom-out"/>
+            <span>缩 小</span></a-button>
           <a-button value="default" @click="handleSizeChange"><span>原始大小</span></a-button>
-          <a-button value="magnify" @click="handleSizeChange"><a-icon type="zoom-in" /><span>放 大</span></a-button>
+          <a-button value="magnify" @click="handleSizeChange">
+            <a-icon type="zoom-in"/>
+            <span>放 大</span></a-button>
         </a-button-group>
       </div>
       <iframe
@@ -25,68 +39,77 @@
 </template>
 
 <script>
-import Vue from 'vue'
-export default {
-  data () {
-    return {
-      title: this.$route.query.title,
-      value: '',
-      size: 1.0,
-      sizeNum: ''
-    }
-  },
-  computed: {},
-  beforeCreate () {
-    if (this.$route.query.title === '2018年韶关市翁源县江尾镇高标准农田建设项目 - 计划阶段 - 任务及投资情况表') {
-      document.title = '任务及投资情况表 - 计划阶段 - 2018年韶关市翁源县江尾镇高标准农田建设项目'
-    } else {
-      document.title = '基本情况表 - 计划阶段 - 2018年韶关市翁源县江尾镇高标准农田建设项目'
-    }
-  },
-  methods: {
-    handleSizeChange (e) {
-      console.log('cs:' + e)
-      if (e.target.value === 'narrow') {
-        this.size -= 0.1
-      } else if (e.target.value === 'default') {
-        this.size = 1.0
-      } else if (e.target.value === 'magnify') {
-        console.log('1')
-        this.size += 0.1
+  import Vue from 'vue'
+  import $ from 'jquery'
+  import '@/utils/bup'
+
+  export default {
+    data() {
+      return {
+        title: this.$route.query.title,
+        value: '',
+        size: 1.0,
+        sizeNum: ''
       }
-      this.set()
-      this.accessData()
     },
-    accessData () {
-      // 存取
-      Vue.ls.remove('setSize')
-      Vue.ls.set('setSize', parseFloat(this.size))
+    computed: {},
+    beforeCreate() {
+      if (this.$route.query.title === '2018年韶关市翁源县江尾镇高标准农田建设项目 - 计划阶段 - 任务及投资情况表') {
+        document.title = '任务及投资情况表 - 计划阶段 - 2018年韶关市翁源县江尾镇高标准农田建设项目'
+      } else {
+        document.title = '基本情况表 - 计划阶段 - 2018年韶关市翁源县江尾镇高标准农田建设项目'
+      }
     },
-    set () {
-      // change size
-      this.$refs.text.style.zoom = this.size
-      this.$refs.text.style.cssText +=
-        '; -moz-transform: scale(' + this.size + ');-moz-transform-origin: 0 0; '
-      this.$refs.text.style.cssText +=
-        '; -webkit-transform: scale(' + this.size + '); -webkit-transform-origin: 0 0; '
+    methods: {
+      bigScale() {
+        setTimeout(function() {
+          $('#app').BUP(1.5, 'App')
+        }, 300)
+      },
+      handleSizeChange(e) {
+        console.log('cs:' + e)
+        if (e.target.value === 'narrow') {
+          this.size -= 0.1
+        } else if (e.target.value === 'default') {
+          this.size = 1.0
+        } else if (e.target.value === 'magnify') {
+          console.log('1')
+          this.size += 0.1
+        }
+        this.set()
+        this.accessData()
+      },
+      accessData() {
+        // 存取
+        Vue.ls.remove('setSize')
+        Vue.ls.set('setSize', parseFloat(this.size))
+      },
+      set() {
+        // change size
+        this.$refs.text.style.zoom = this.size
+        this.$refs.text.style.cssText +=
+          '; -moz-transform: scale(' + this.size + ');-moz-transform-origin: 0 0; '
+        this.$refs.text.style.cssText +=
+          '; -webkit-transform: scale(' + this.size + '); -webkit-transform-origin: 0 0; '
+      }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
-h1 {
-  margin: 0;
-}
-/deep/ .ant-card {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  /deep/ .ant-card-head {
-    height: 62px;
+  h1 {
+    margin: 0;
   }
-  /deep/ .ant-card-body {
-    flex: auto;
+
+  /deep/ .ant-card {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    /deep/ .ant-card-head {
+      height: 62px;
+    }
+    /deep/ .ant-card-body {
+      flex: auto;
+    }
   }
-}
 </style>
